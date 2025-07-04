8778a625ede6e67c8aec696bcdfd05498a00ce37
Dudekula Rajesh <145743563+Dudekularajesh@users.noreply.github.com>
2025-07-04 20:05:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-const form = document.querySelector('.contact-form');
-if (form) {
-  form.addEventListener('submit', () => {
-    setTimeout(() => {
-      alert('Thank you for your message! I will get back to you soon.');
-    }, 1000);
-  });
-}
+// const form = document.querySelector('.contact-form');
+// if (form) {
+//   form.addEventListener('submit', () => {
+//     setTimeout(() => {
+//       alert('Thank you for your message! I will get back to you soon.');
+//     }, 1000);
+//   });
+// }
 
